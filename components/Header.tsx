5127815af64a57350fdeb7fdb7e661961be09354
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-12-14 18:02:52 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,30 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/solid';
 
 type Props = {};
 
 export default function Header({}: Props) {
 	return (
-		<header className="sticky top-0 ">
+		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 			<div className="relative flex items-center h-10 cursor-pointer my-auto">
 				<Image
-					src="/Airbnb-logo.jpg"
-					width={200}
-					height={200}
+					src="/airbnb-logo.png"
 					alt="logo"
+					width={120}
+					height={100}
 					object-fit="contain"
 					object-position="left"
 				/>
 			</div>
 
-			<div></div>
+			<div className="flex items-center border-2 rounded-full">
+				<input type="text" placeholder="start your search" className="h-8" />
+				<SearchIcon className="h-8 hidden md:inline-flex text-white bg-red-400 rounded-full p-1 cursor-pointer" />
+			</div>
 
 			<div></div>
 		</header>
 	);
 }
+

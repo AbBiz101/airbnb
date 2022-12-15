6ff3bfd69a821116349068e6f51d65917592cd11
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-12-15 21:25:12 +0100

    Update LiveAnywhereSection.tsx

--- components/LiveAnywhereSection.tsx
+++ components/LiveAnywhereSection.tsx
@@ -7,7 +7,7 @@ export default function LiveAnywhereSection({ img, title }: Props) {
 	return (
 		<div className="cursor-pointer hover:scale-105 transform translate duration-300 ease-out">
 			<div className="relative h-80 w-80">
-				<Image src={img} layout="fill" className="rounded-xl" alt={title} />
+				<Image src={img} fill sizes="200px" className="rounded-xl" alt={title} />
 			</div>
 			<h3 className="text-2xl text-bold">{title}</h3>
 		</div>

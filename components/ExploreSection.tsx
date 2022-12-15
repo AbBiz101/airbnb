34a039f8b6c9900347af59d26b9326ebd5c32957
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-12-15 21:25:21 +0100

    Update ExploreSection.tsx

--- components/ExploreSection.tsx
+++ components/ExploreSection.tsx
@@ -10,7 +10,7 @@ export default function ExploreSection({ img, location, distance }: Props) {
 	return (
 		<div className="flex items-center m-2 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 translate transform duration-200 ease-out">
 			<div className="relative h-16 w-16">
-				<Image src={img} layout="fill" className="rounded-lg" alt={location} />
+				<Image src={img} fill className="rounded-lg" alt={location} />
 			</div>
 			<div>
 				<h2>{location}</h2>

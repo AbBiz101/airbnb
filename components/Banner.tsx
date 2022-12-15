e8b95b5f29a66950e8a1e2ac196025ca864d6dc7
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-12-15 21:25:26 +0100

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 export default function Banner({}: Props) {
 	return (
-		<div className="relative h-[250px] sm:h-[350px] md:h-[450px] lg:h-[550px] xl:h-[650px] 2xl:h-[750px] max-w-[2300px]">
-			<Image src="/banner_1.jpg" layout="fill" alt="" object-fit="cover" />
+		<div className="relative h-[250px] sm:h-[350px] md:h-[450px] lg:h-[550px] xl:h-[650px] 2xl:h-[750px] ">
+			<Image src="/banner_1.jpg" fill alt="" object-fit="cover" />
 			<div className="absolute text-center top-1/2 w-full">
 				<p className="text-sm sm:text-lg md:text-xl lg:text-2xl ">
 					Not sure where to go? perfect.

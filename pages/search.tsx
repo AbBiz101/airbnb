50076e328e9d5f9643c406ffb0c5b8719c3e6863
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-12-16 21:01:27 +0100

    Update search.tsx

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { format } from 'date-fns';
+import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
 type Props = {};
 
 export default function search({}: Props) {
+	const router = useRouter();
+	const { location, startDate, endDate, guestInput } = router.query;
+	
+	//const formatedStartDate2 = format(new Date(), 'dd MMMM yy');
+	//const formatedStartDate = format(new Date(startDate), 'dd MMMM yy');
+	//const formatedEndDate = format(new Date(endDate), 'dd MMMM yy');
+	//console.log(new Date(), startDate);
+
 	return (
 		<div>
 			<Header />
 			<main>
 				<section className="bg-red-200 flex-grow pt-14 px-6">
-					<p className="text-xs">Lorem ipsum dolor sit amet.</p>
-					<h1 className="text-3xl font-semibold mt-2 mb-6">
-						Lorem ipsum dolor.
-					</h1>
+					{location && (
+						<>
+							<p className="text-xs">
+								200+ Stays {startDate}- {endDate} - for
+								{guestInput} of guests.
+							</p>
+							<h1 className="text-3xl font-semibold mt-2 mb-6">
+								Stay in {location}.
+							</h1>
+						</>
+					)}
 
 					<div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
 						<p className="button">Cancellation Flexibility</p>
